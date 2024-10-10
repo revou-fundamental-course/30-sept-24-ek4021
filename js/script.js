24f8461ce21cde9d6462b3fb3164d8ad63f14c1c
Eka Prasetya <eka.prasetya021@gmail.com>
2024-10-10 20:15:35 +0700

    Konfigurasi ketiga mini project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 // ini javascript
+
+console.log('Javascript tersambung')
+
+function validateForm(){
+    console.log('Validasi berhasil terpanggil');
+    const inputSuhu = document.getElementById('input-suhu').value;
+
+    if (inputSuhu == '') {
+        alert('Inputan anda kosong');
+    }else{
+        calculate(inputSuhu);
+    }
+        
+
+}
+
+function calculate(value) {
+    let result = parseFloat(value*9/5) + 32;
+    document.getElementById('hasil-suhu').value = result;
+    document.getElementById('how-to-suhu').value = '(' + value + ' x 9/5) + 32 = ' + result;
+}
+
+
+function reset() {
+
+    document.getElementById('input-suhu').value = '' ;
+    document.getElementById('hasil-suhu').value = '' ;
+    document.getElementById('how-to-suhu').value = '' ;
+}
